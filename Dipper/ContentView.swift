4ea58ef80f2b4c27855683b52693720cea17483b
rajtailor96 <rajtailor96@gmail.com>
2021-09-12 19:28:34 -0500

    Initial Commit

---
+++ Dipper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dipper
+//
+//  Created by Raj Tailor on 9/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dipper/DipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DipperApp.swift
+//  Dipper
+//
+//  Created by Raj Tailor on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
